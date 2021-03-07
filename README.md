e9879bd6e1052dbf5e3da8b4cf21e5bdf8c1c9e4
samiul1988 <samiulhaydereee@gmail.com>
2021-03-07 16:22:30 -0700

    finished logic

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# assignment13_e_commerce_backend
+## Assignment 13: E-commerce Back End
+---
+### Topic
+Object Relational Mapping (ORM)
+
+### User Story (Obtained from the assignment description)
+
+```
+AS A manager at an internet retail company
+I WANT a back end for my e-commerce website that uses the latest technologies
+SO THAT my company can compete with other e-commerce companies
+```
+
+### Acceptance Criteria (Obtained from the assignment description)
+
+```
+GIVEN a functional Express.js API
+WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+THEN I am able to connect to a database using Sequelize
+WHEN I enter schema and seed commands
+THEN a development database is created and is seeded with test data
+WHEN I enter the command to invoke the application
+THEN my server is started and the Sequelize models are synced to the MySQL database
+WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+THEN I am able to successfully create, update, and delete data in my database
+```
+
+## My Actions and Notes
+
+* The project was developed from the code base provided in the assignment description page ([link](https://github.com/coding-boot-camp/fantastic-umbrella)).
+* Basic considerations were as follows:
+    * Following packages were used in the assignment: ```MySQL2```, ```Sequelize``` and ```dotenv``` 
+    * The database configuration information was saved in a local ```.env``` file
+    * No test files have been added since there was no specific instruction to add test cases
+    * The ```db/schema.sql``` file was used to create the database using MySQL shell command
+    * Demo data was inserted using a ```npm run seed``` command 
+    * Database models and associations were created as instructed in the assignment description. 
+    * The Sequelize models were synced before running the server.  
+
+
+### Demo Run
+![Demo Run](./assets/images/assignment13_demo.gif)
+
+### Link of Walkthrough Video
+[Click here to see the video](https://drive.google.com/file/d/1iTvvTkOHUni69uoxxFCpF1RsHJNWwP5V/view)
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,25 +4,140 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+    // find all tags
+    // be sure to include its associated Product data
+    Tag.findAll({
+        include: [
+            {
+                model: Product,
+                through: ProductTag
+            }
+        ]
+    })
+    .then(dbTagData => res.status(200).json(dbTagData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
+    Tag.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Product,
+                through: ProductTag
+            }
+        ]
+    })
+    .then(dbTagData => {
+        if (!dbTagData) {
+            res.status(404).json({ message: 'No tag found with this id' });
+            return;
+        }
+        res.json(dbTagData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+    // create a new tag
+    /* req.body should look like this...
+      {
+        tag_name: "yellow",
+        productIds: [1, 2, 4]
+      }
+    */
+   Tag.create(req.body)
+        .then((tag) => {
+            // if there's a list of products the tag is associated with, 
+            // then create pairings to bulk create in the ProductTag model
+            if (req.body.productIds.length) {
+                const productTagIdArr = req.body.productIds.map((product_id) => {
+                    return {
+                        product_id,
+                        tag_id: tag.id,
+                    };
+                });
+                return ProductTag.bulkCreate(productTagIdArr);
+            }
+            // if no product list, just respond
+            res.status(200).json(tag);
+        })
+        .then((productTagIds) => res.status(200).json(productTagIds))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+    // update a tag's name by its `id` value
+    Tag.update(req.body, {
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then((tag) => {
+            // find all associated proucts from ProductTag
+            return ProductTag.findAll({ where: { tag_id: req.params.id } });
+        })
+        .then((productTags) => {
+            // get list of current product_ids
+            const productTagIds = productTags.map(({ product_id }) => product_id);
+            // create filtered list of new product_ids
+            const newProductTags = req.body.productIds
+                .filter((product_id) => !productTagIds.includes(product_id))
+                .map((product_id) => {
+                    return {
+                        product_id,
+                        tag_id: req.params.id,
+                    };
+                });
+            // figure out which ones to remove
+            const productTagsToRemove = productTags
+                .filter(({ product_id }) => !req.body.productIds.includes(product_id))
+                .map(({ id }) => id);
+
+            // run both actions
+            return Promise.all([
+                ProductTag.destroy({ where: { id: productTagsToRemove } }),
+                ProductTag.bulkCreate(newProductTags),
+            ]);
+        })
+        .then((updatedProductTags) => res.json(updatedProductTags))
+        .catch((err) => {
+            // console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+    // delete on tag by its `id` value
+    Tag.destroy({
+        where: {
+          id: req.params.id
+        }
+    })
+    .then(dbTagData => {
+        if (!dbTagData) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+        }
+        res.json(dbTagData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
